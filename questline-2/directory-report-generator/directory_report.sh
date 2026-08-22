c10fbc8d6e8ecae8568db52006226d26d5f7104e
Adwaitht <adwaithadwa2007@gmail.com>
2026-08-22 22:11:38 +0530

    Update directory_report.sh

--- questline-2/directory-report-generator/directory_report.sh
+++ questline-2/directory-report-generator/directory_report.sh
@@ -4,7 +4,7 @@ dir=$1
 
 echo "Directory Report" > report.txt
 echo "=================" >> report.txt
-echo "Total number of files: $(find "$dir" -type f | wc -l)" >> report.txt
+echo "Total number of files: $(find "$dir" -type f ! -name report.txt | wc -l)" >> report.txt
 echo "Total number of executable files: $(find "$dir" -type f ! -name report.txt -executable | wc -l)" >> report.txt
 echo "Largest file: $(find "$dir" -type f ! -name report.txt -printf '%s %p\n' | sort -nr | head -1)" >> report.txt
 echo "Most recently modified file: $(find "$dir" -type f ! -name report.txt -printf '%T@ %p\n' | sort -nr | head -1)" >> report.txt
